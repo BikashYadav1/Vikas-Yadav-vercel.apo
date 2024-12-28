c3030a250fb314b89dca19a556ffeaa9ee057540
BikashYadav <by190545@gmail.com>
2024-12-28 21:48:15 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Get the gallery section
+const gallery = document.querySelector('.gallery');
+
+// Number of images
+const totalImages = 58;
+
+// Dynamically add images to the gallery
+for (let i = 1; i <= totalImages; i++) {
+  const imgDiv = document.createElement('div');
+  imgDiv.classList.add('gallery-item');
+  imgDiv.innerHTML = `<img src="Nitin-Portfolio_${i}.jpg" alt="Portfolio Image ${i}">`;
+  gallery.appendChild(imgDiv);
+}
